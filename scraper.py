29e58924280888b19cfa32ba9b4a146afa48c914
Kronus2925 <92937896+Kronus2925@users.noreply.github.com>
2022-09-07 12:41:06 +0200

    scraper update
    adding selenium functionality to scroll page and click button

--- scraper.py
+++ scraper.py
@@ -1,6 +1,12 @@
-from requests import get
+from lib2to3.pgen2 import driver
 from bs4 import BeautifulSoup
+from requests import get
 import os
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
 
 def main_logic():
 
@@ -21,14 +27,45 @@ def main_logic():
         os.chdir(os.path.join(os.getcwd(), userchoice_foldername))
 
     url = f'https://unsplash.com/s/photos/{userchoice_photo}'
-
-    imgs = html_acces(url)
+    driver_setup(url)
+    driver = driver_setup(url)
+    button_click(driver)
+    site_scrolling(driver)
+    imgs = html_acces(driver)
     image_download(imgs, userchoice_count)
 
-def html_acces(url):
+def driver_setup(url):
+
+    driver = webdriver.Safari()
+    driver.maximize_window()
+    driver.get(url)
+    return(driver)
     
-    page = get(url)
-    soup = BeautifulSoup(page.content, 'lxml')
+def button_click(driver):
+
+    btn = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//button[text()="Load more photos"]')))
+    btn.click()    
+
+def site_scrolling(driver):
+    
+    time.sleep(2)
+    scroll_pause_time = 1 
+    screen_height = driver.execute_script("return window.screen.height;")  
+    i = 1
+
+    while True:
+        
+        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+        i += 1
+        time.sleep(scroll_pause_time)
+        scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+        if (screen_height) * i > scroll_height:
+            break 
+
+def html_acces(driver):
+
+    soup = BeautifulSoup(driver.page_source, 'lxml')
     imgs = soup.find_all('img',{'class' : 'YVj9w'}, alt=True)
     return imgs
 
@@ -43,5 +80,5 @@ def image_download(imgs, userchoice_count):
                 im = get(lnk)
                 f.write(im.content)
                 print('Saving: ' + name + '.jpg')
-        
+    
 main_logic()
