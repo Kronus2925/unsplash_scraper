e0cc68a4fc3ba4063868cad42619a160eb8bf19e
Kronus2925 <92937896+Kronus2925@users.noreply.github.com>
2022-09-12 19:31:31 +0200

    final version
    adding final version of the project

--- scraper.py
+++ scraper.py
@@ -7,10 +7,12 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 
-def main_logic():
 
-    userchoice_photo = input("What are you searching for ?\n")
-    userchoice_foldername = userchoice_photo
+def main():
+
+    userchoice_foldername = input("What are you searching for ?\n")
+
+    url = f'https://unsplash.com/s/photos/{userchoice_foldername}'
 
     while True:
         try:
@@ -20,17 +22,14 @@ def main_logic():
             print("You must provide a number !\n")
     try:
         os.mkdir(os.path.join(os.getcwd(), userchoice_foldername))
-        os.chdir(os.path.join(os.getcwd(), userchoice_foldername))
-    except:
+    except FileExistsError:
         print('Failed to create directory, provided path already exists\n')
-        os.chdir(os.path.join(os.getcwd(), userchoice_foldername))
-
-    url = f'https://unsplash.com/s/photos/{userchoice_photo}'
     
-    driver_setup(url)
+    os.chdir(os.path.join(os.getcwd(), userchoice_foldername))
+
     driver = driver_setup(url)
     button_click(driver)
-    site_scrolling(driver)
+    site_scrolling(driver,userchoice_count)
     imgs = html_acces(driver)
     image_download(imgs, userchoice_count)
 
@@ -39,15 +38,15 @@ def driver_setup(url):
     driver = webdriver.Safari()
     driver.maximize_window()
     driver.get(url)
-    return(driver)
-    
+    return driver
+
 def button_click(driver):
 
     btn = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//button[text()="Load more photos"]')))
     btn.click()    
 
-def site_scrolling(driver):
+def site_scrolling(driver,userchoice_count):
     
     time.sleep(2)
     scroll_pause_time = 1 
@@ -55,12 +54,13 @@ def site_scrolling(driver):
     i = 1
 
     while True:
-        
-        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+
+        driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i)) 
         i += 1
         time.sleep(scroll_pause_time)
         scroll_height = driver.execute_script("return document.body.scrollHeight;")  
-        if (screen_height) * i > scroll_height:
+        image_count = driver.find_elements(By.XPATH,'//img[@class="YVj9w"][@alt]')
+        if (screen_height) * i > scroll_height or len(image_count) > userchoice_count:
             break 
 
 def html_acces(driver):
@@ -69,6 +69,7 @@ def html_acces(driver):
     imgs = soup.find_all('img',{'class' : 'YVj9w'}, alt=True)
     return imgs
 
+
 def image_download(imgs, userchoice_count):
 
     for idx, image in enumerate(imgs):
@@ -76,9 +77,11 @@ def image_download(imgs, userchoice_count):
         if  idx < userchoice_count:
             lnk = image['src']
             name = image['alt']
-            with open(name.replace(' ', '-').replace('/', '') + '.jpg', 'wb') as f:
+            ext_type = get(lnk).headers['content-type'].replace('image/', '.')
+            with open(name.replace(' ', '-').replace('/', '') + ext_type, 'wb') as f:
                 im = get(lnk)
                 f.write(im.content)
-                print('Saving: ' + name + '.jpg')
-    
-main_logic()
+                print('Saving: ' + name + ext_type)
+
+if __name__ == '__main__':    
+    main()
